ef685f173536122e6a8a37382620e1d104f2b333
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-02-16 17:38:38 +0100

    2.2

---
+++ control-structures/2.2.js
@@ -0,0 +1,14 @@
+const readlineSync=require("readline-sync");
+
+let min= new Number(readlineSync.question("Choose a min number"));
+let max= new Number(readlineSync.question("Choose a max number"));
+if (min > max){
+    console.log("Error: You didn't understand anything, try again");
+}else{ 
+let current= new Number(readlineSync.question("Good, now choose a current number"));
+if (min>current){
+    console.log(current);
+}else if (current>max){
+    console.log(current);
+}
+}
