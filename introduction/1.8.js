43704856c24a7d7bd1d9bff9bbca8c31c43aa538
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-02-16 16:30:40 +0100

    1.8

---
+++ introduction/1.8.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+function questions() {
+  let question = 4;
+  let one = readlineSync.question("blablabla ?" + ("Questions " + question + " : "));
+  --question;
+  let two = readlineSync.question("blablablablabla ?!" + ("Questions " + question + " : "));
+  --question;
+  let three = readlineSync.question("blablabla ?" + ("Questions " + question + " : "));
+  --question;
+  let four = readlineSync.question("bla?" + ("Questions : " + question + " : "));
+  --question;
+  console.log("Blablablabla blababla " + one +" blabababla "+ two+ " bla " + three + "babla" + four + " blablabahahah !");
+}
+questions();
