3c328a4ca5c307c8113530d4dce78ac39e340757
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-02-16 11:34:34 +0100

    1.1.0

---
+++ introduction/1.1.0.js
@@ -0,0 +1,11 @@
+let ageOfCat = 3; // let variable changeable 
+const numberOfLife = 7; // Const variable inchangeable 
+let RealAgeCat = "Votre chat à " + ageOfCat * numberOfLife + " ans";
+console.log(RealAgeCat);  
+console.log(14*3); // 14 * 3 = 42
+console.log(8/2); // 8 / 4 = 4 
+console.log(5+3); // 5 + 3 = 8 
+console.log(10-4); // 10 - 4 = 6
+console.log(10%3); // 10 % 3 = 1 (3*3+'1')
+
+
