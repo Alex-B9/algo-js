2fbae1701c2c0f17cb503ac937a9b5ff3cead280
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-02-17 10:16:28 +0100

    2.3

---
+++ control-structures/2.3.js
@@ -0,0 +1,12 @@
+const readlineSync=require("readline-sync");
+let nbr = 0
+
+while (nbr <=100){
+    console.log(nbr);
+    nbr+=2;
+}if (nbr > 100){
+console.log("Good, now *50");
+}
+for (let nbr = 0; nbr < 51; nbr+=2){
+    console.log(nbr);
+}
